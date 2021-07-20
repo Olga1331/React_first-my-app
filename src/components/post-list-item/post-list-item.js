6213124fdfd280a1950463dde8ad35c78b1ca376
Olga <chadaeva88@gmail.com>
2021-07-20 16:46:40 +0300

    add onClick to btn:star,heart,trash

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './post-list-item.css';
 
-const PostListItem = ({label, important = false}) => {
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        important: false,
+        like: false
+    };
+    this.onImportant = this.onImportant.bind(this);
+    this.onLike = this.onLike.bind(this);
+}
 
+    onImportant() {
+        this.setState(({important}) => ({/*  навешиваем обрабочкик событий */
+            important: !important
+        }))
+    }
 
+    onLike() {
+        this.setState(({like}) => ({/*  навешиваем обрабочкик событий */
+            like: !like
+        }))
+    }
 
-    return (
-        <li className='app-list-item d-flex justify-content-between'>
-            <span className='app-list-item-label'>
+    render () {
+        const {label, onDelete} = this.props;
+        const {important, like} = this.state;
+        let classNames='app-list-item d-flex justify-content-between';
+
+        if (important) { /* если передано значение,то добавляем класс important для звездочки */
+            classNames +=" important"; /* обязательно ставим пробел,т.к. формируем строку в котоорой между классами есть пробелы. */
+        }
+
+        if (like) {
+            classNames +=' like';
+        }
+
+        return(
+            <div className={classNames}>
+            <span className='app-list-item-label'
+            onClick={this.onLike}>
             {label}
             </span>
             <div className='d-flex justify-content-center align-items-center'>
-                <button type='buttton' className='btn-star btn-sm'>
+                <button type='buttton' className='btn-star btn-sm' 
+                onClick={this.onImportant}>
                     <i className='fa fa-star'></i>
                 </button>
-                <button type='buttton' className='btn-trash btn-sm'>
-                    <i className='fa fa-trash'></i>
+                <button type='buttton' className='btn-trash btn-sm'
+                onClick={onDelete}>
+                    <i className='fa fa-trash'></i>  
                 </button>
                     <i className='fa fa-heart'></i>
             </div>
 
-        </li>
-    )
+        </div>
+        )
+    }
 }
-export default PostListItem;
+
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,16 +2,24 @@ import React from 'react';
 import PostListItem from '../post-list-item/post-list-item';
 import './post-list.css';
 
-const PostList = () => {
-    return (
-        <div>
-            <ul className="app-list list-group">
-                <PostListItem label='Going to learn react' important/>
-                <PostListItem label='That is so good'/>
-                <PostListItem label='I need a break...'/>
+const PostList = ({posts, onDelete}) => {
 
-            </ul>
-        </div>
+    const elements = posts.map((item) => { /* перебираем массив данных, котор пришли с сервера */
+       
+        const {id, ...itemProps} = item;
+       
+        return ( /* и возвращаем новый массив */
+            <li key={id} className='list-group-item'>
+                <PostListItem {...itemProps}   /* label, important */ 
+                onDelete={() => onDelete(id)}/>
+            </li>
+        )
+    });
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>   
     )
 }
 export default PostList;
